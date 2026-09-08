12978c4e731b181c3eba6431986b958fe61d5388
Paulina <pescudero@cread.org.pe>
2026-09-08 14:32:29 -0300

    Crear frontend Pethub con Angular Ionic y Capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.pethub.app',
+  appName: 'Pethub',
+  webDir: 'www'
+};
+
+export default config;
